f389bfc2f2fdc4e719faa26eb920bfe67cd5ac1a
Alfonso Luis Del Rosario <addelros@ucsc.edu>
2023-02-23 18:13:46 -0800

    switch to vscode

---
+++ app/mens_health_app/android/app/src/main/kotlin/com/example/mens_health_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mens_health_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- app/onlymans/android/app/src/main/kotlin/com/example/onlymans/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.onlymans
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
