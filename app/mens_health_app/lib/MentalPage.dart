3c7d47536fd9924d83a8a24ee4b3caf8920578a4
RTryantaylor <114191636+RTryantaylor@users.noreply.github.com>
2023-03-12 05:31:53 -0700

    Update MentalPage.dart

--- app/mens_health_app/lib/MentalPage.dart
+++ app/mens_health_app/lib/MentalPage.dart
@@ -1,129 +1,113 @@
 import 'all.dart';
-import 'dart:math';
-import 'package:flutter/material.dart';
-
-class DiamondContainer extends StatelessWidget {
-  final double size;
-  final Color color;
-
-  const DiamondContainer({required this.size, required this.color});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: size,
-      height: size,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            color.withOpacity(0.8),
-            color.withOpacity(0.5),
-            color.withOpacity(0.8),
-          ],
-        ),
-        shape: BoxShape.circle,
-      ),
-      child: Center(
-        child: Icon(
-          Icons.star,
-          size: size * 0.6,
-          color: Colors.white,
-        ),
-      ),
-    );
-  }
-}
 
 class MentalPage extends StatefulWidget {
   const MentalPage({Key? key}) : super(key: key);
-
   @override
-  MentalPageState createState() => MentalPageState();
+  PhysicalPageState createState() => PhysicalPageState();
 }
 
 class MentalPageState extends State<MentalPage> {
+  Widget _buildList(double h, double w) {
+    return PageView(
+        scrollDirection: Axis.vertical,
+        children: [pageOne(h, w), pageTwo(h, w), pageThree(h, w)]);
+  }
+
+  Container pageOne(double h, double w) {
+    const String challengeOne = '''
+A random paragraph can also be an excellent way for a writer to tackle writers block. Writing block can often happen due to being stuck with a current project that the writer is trying to complete. By inserting a completely random
+''';
+
+    return Container(
+        color: const Color.fromARGB(255, 30, 41, 82),
+        child: Center(
+            child: Container(
+                color: const Color.fromARGB(255, 20, 31, 72),
+                height: h,
+                width: w,
+                margin: const EdgeInsets.only(left: 10, right: 10),
+                child: Container(
+                    margin: const EdgeInsets.only(
+                        top: 95, right: 10, left: 10, bottom: 10),
+                    child: const Text(challengeOne,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Color.fromARGB(255, 237, 171, 26),
+                          fontFamily: 'Times New Roman',
+                        ))))));
+  }
+
+  Container pageTwo(double h, double w) {
+    return Container(
+        color: const Color.fromARGB(255, 30, 41, 82),
+        child: Center(
+            child: Container(
+                color: const Color.fromARGB(255, 20, 31, 72),
+                height: h,
+                width: w,
+                margin: const EdgeInsets.only(
+                    top: 0, right: 10, left: 10, bottom: 10),
+                child: const Text('jdkalsdjsak'))));
+  }
+
+  Container pageThree(double h, double w) {
+    return Container(
+        color: const Color.fromARGB(255, 30, 41, 82),
+        child: Center(
+            child: Container(
+                color: const Color.fromARGB(255, 20, 31, 72),
+                height: h + 30,
+                width: w - 30.0,
+                margin: const EdgeInsets.only(
+                    top: 110, right: 10, left: 10, bottom: 10),
+                child: const Text(''))));
+  }
+
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    var padding = MediaQuery.of(context).padding;
+    double newheight = height - padding.top - padding.bottom;
+
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(75),
-        child: Container(
-          color: Colors.white,
-          padding: const EdgeInsets.only(top: 40),
-          alignment: Alignment.topCenter,
-          child: Column(
-            children: [
-              const Text(
-                'Mental',
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 30, 41, 82),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: const [
+            Expanded(child: Icon(Icons.star_border)),
+            SizedBox(width: 10),
+            Expanded(child: Icon(Icons.star_border)),
+            SizedBox(width: 10),
+            Expanded(child: Icon(Icons.star_border)),
+            SizedBox(width: 10),
+            Flexible(
+              flex: 10,
+              child: Text(
+                'Mental Health',
                 style: TextStyle(
-                  fontSize: 24,
+                  fontSize: 25,
+                  color: Color.fromARGB(255, 237, 171, 26),
+                  fontFamily: 'Courier',
                   fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 177, 37, 37),
                 ),
+                textAlign: TextAlign.center,
               ),
-              Container(
-                decoration: const BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Colors.black,
-                      width: 2.0,
-                    ),
-                  ),
-                ),
-                child: const Text(
-                  'Challenges',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ),
-      body: Container(
-        color: const Color.fromARGB(255, 182, 30, 30),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  DiamondContainer(
-                    size: 50,
-                    color: Colors.white,
-                  ),
-                  SizedBox(width: 10),
-                  DiamondContainer(
-                    size: 50,
-                    color: Colors.white,
-                  ),
-                  SizedBox(width: 10),
-                  DiamondContainer(
-                    size: 50,
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              const TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: 'Enter text here',
-                  border: OutlineInputBorder(),
-                  contentPadding: EdgeInsets.all(20),
-                ),
-              ),
-            ],
-          ),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(4.0),
+          child: Container(color: Colors.orange, height: 2.0),
         ),
+        toolbarHeight: 70,
+        centerTitle: false,
       ),
+      extendBodyBehindAppBar: true,
+      body: _buildList(newheight, width),
     );
   }
 }
