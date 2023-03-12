cc192c05b295e031d3615bcb559b4915bf02192e
Vishruth Bharath <31827869+vishruthb@users.noreply.github.com>
2023-03-11 16:05:15 -0800

    redo for functionality
    redo for functionality still need to connect it somehow

--- app/mens_health_app/lib/HomePage.dart
+++ app/mens_health_app/lib/HomePage.dart
@@ -1,38 +1,62 @@
-import 'all.dart';
+import 'dart:html';
 
-class HomePage extends StatefulWidget {
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
-  @override
-  HomePageState createState() => HomePageState();
-}
-
-class HomePageState extends State<HomePage> {
-  final PageController _home = PageController(initialPage: 2);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        controller: _home,
-        children: [
-          // Friends
-          const FriendsPage(),
-          // Mental
-          const MentalPage(),
-          // Center page
-          Container(
-            color: Colors.yellow,
-            child: const CustomScrollView(
-              slivers: [],
+      backgroundColor: Colors.black, // set background color to black
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'ponder', // text for "ponder"
+              style: TextStyle(
+                fontFamily: 'Ponder',
+                fontWeight: FontWeight.w400, // set font weight to regular
+                fontSize: 48.0,
+                color: Colors.white,
+              ),
             ),
-          ),
-          // Physical
-          const PhysicalPage(),
-          // Articles
-          const ArticlesPage(),
-        ],
+            Text(
+              'box', // text for "box"
+              style: TextStyle(
+                fontFamily: 'Ponder',
+                fontWeight: FontWeight.w700, // set font weight to bold
+                fontSize: 48.0,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 32.0), // add some spacing between logo and button
+            ElevatedButton(
+              onPressed: () {
+                // TODO: add sign-in functionality
+              },
+              child: Text(
+                'Sign In', // text for sign-in button
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                onPrimary: Colors.black,
+                padding: EdgeInsets.symmetric(horizontal: 64.0, vertical: 16.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32.0),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+class MainAxisAlignment {}
