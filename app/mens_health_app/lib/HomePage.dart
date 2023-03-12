65da78da66932ef8ff05d7a4dc77c26eb546508b
alfonsodelr <61682073+alfonsodelr@users.noreply.github.com>
2023-03-11 21:47:00 -0800

    Update HomePage.dart

--- app/mens_health_app/lib/HomePage.dart
+++ app/mens_health_app/lib/HomePage.dart
@@ -1,62 +1,38 @@
-import 'dart:html';
+import 'all.dart';
 
-import 'package:flutter/material.dart';
-
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  final PageController _home = PageController(initialPage: 2);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black, // set background color to black
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'ponder', // text for "ponder"
-              style: TextStyle(
-                fontFamily: 'Ponder',
-                fontWeight: FontWeight.w400, // set font weight to regular
-                fontSize: 48.0,
-                color: Colors.white,
-              ),
+      body: PageView(
+        controller: _home,
+        children: [
+          // Friends
+          const FriendsPage(),
+          // Mental
+          const MentalPage(),
+          // Center page
+          Container(
+            color: Colors.yellow,
+            child: const CustomScrollView(
+              slivers: [],
             ),
-            Text(
-              'box', // text for "box"
-              style: TextStyle(
-                fontFamily: 'Ponder',
-                fontWeight: FontWeight.w700, // set font weight to bold
-                fontSize: 48.0,
-                color: Colors.white,
-              ),
-            ),
-            SizedBox(height: 32.0), // add some spacing between logo and button
-            ElevatedButton(
-              onPressed: () {
-                // TODO: add sign-in functionality
-              },
-              child: Text(
-                'Sign In', // text for sign-in button
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.white,
-                onPrimary: Colors.black,
-                padding: EdgeInsets.symmetric(horizontal: 64.0, vertical: 16.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(32.0),
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+          // Physical
+          const PhysicalPage(),
+          // Articles
+          const ArticlesPage(),
+        ],
       ),
     );
   }
 }
-
-class MainAxisAlignment {}
